5c15ea0286f6c6f09ad2f7d75466e6e65e202923
Gio Oliveira <giothais.silva@gmail.com>
2025-05-20 19:43:24 -0300

    feat: initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="w-screen inline-flex p-4 bg-blue-500 mb-8 shadow-md">
+      <h1 className="text-white font-bold">Gestão de pedidos</h1>
+    </header>
+  );
+};
+
+export default Header;
